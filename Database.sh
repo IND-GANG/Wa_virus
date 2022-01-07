663258b123e13d3d4dc863752f55ec459c1b033d
ADIL <90197148+IND-GANG@users.noreply.github.com>
2022-01-07 10:54:04 +0530

    Add files via upload

--- Database.sh
+++ Database.sh
@@ -65,227 +65,227 @@ echo -e "\e[1;32m [0] EXIT"
 read ch
 clear
 if [ $ch -eq 1 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/ldnq7s6i752x73r2irwz475nwpfvf3yk5wgy22zvwwzcx76nkp6k27i/1.TRAWA_DATABASE.zip
+  wget https://download1075.mediafire.com/toe9qvg67eng/psqh6elyhhkm5pr/1.TRAWA_DATABASE.zip
   cp -r 1.TRAWA_DATABASE.zip /sdcard
   mv 1.TRAWA_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 2 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/47sa7s6e6ysgl3rtabwz6mvuu5fx5fu77wqblnj5qwf727mrjvvqr4q/2.YAN.ZAPO.zip
+  wget https://download1084.mediafire.com/f09oajxmevug/mjw8wizarql8l7i/2.YAN.ZAPO.zip
   cp -r 2.YAN.ZAPO.zip /sdcard
   mv 2.YAN.ZAPO.zip Downloads
   exit
 elif [ $ch -eq 3 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/4lsa7s4v5yucbm3gajzmyz7ju6mdb3amj6htqxzxitgpp7nkdg3w57y/3.TDN.DATABASE.zip
+  wget https://download1589.mediafire.com/zy6bpf7nvpig/r49e7zbwvjbz23r/3.TDN.DATABASE.zip
   cp -r 3.TDN.DATABASE.zip /sdcard
   mv 3.TDN.DATABASE.zip Downloads
   exit
 elif [ $ch -eq 4 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/57sa7s6c5zzghnjfleuim5xjuobeepxu4hvxl6rko5aj5zzm46444fq/4.DB.FALLEN.V3.zip
+  wget https://download1075.mediafire.com/b3lfg6gs8bsg/vxedm9dvsjdmlz6/4.DB.FALLEN.V3.zip
   cp -r 4.DB.FALLEN.V3.zip /sdcard
   mv 4.DB.FALLEN.V3.zip Downloads
   exit
 elif [ $ch -eq 5 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/53sa7s455qvwf2tekiqzu2pn6fbsqa6cj45rm2dbkbeqbkqw7i36k5q/5.AC_WAR_DATABASE.zip
+  wget https://download1324.mediafire.com/ss0sylir39lg/zou5zgecfj88gy0/5.AC_WAR_DATABASE.zip
   cp -r 5.AC_WAR_DATABASE.zip /sdcard
   mv 5.AC_WAR_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 6 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/5xsa7s4d6uvsrytnavvmg75euba3h3dmtb5pt7walhbaeamvjjh4tcq/6.NIGHTWALKER_DATABASE.zip
+  wget https://download1488.mediafire.com/fvxm3wx1n1qg/6gaaxfo7k8u41zt/6.NIGHTWALKER_DATABASE.zip
   cp -r 6.NIGHTWALKER_DATABASE.zip /sdcard
   mv 6.NIGHTWALKER_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 7 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/5tsa7s4evqwsrobxizqz4z5567hgalevchbhn5gydw7baddjczgp4my/7.TEAM_EZRA.zip
+  wget https://download1481.mediafire.com/xg2prd6olgkg/uqjsbyznxawtwhq/7.TEAM_EZRA.zip
   cp -r 7.TEAM_EZRA.zip /sdcard
   mv 7.TEAM_EZRA.zip Downloads
   exit
 elif [ $ch -eq 8 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/5lsa7s6hub2cro3hkjyjgmpgv73zn4pnp4bdc65hceb24vebvhx7ivy/8.DOUGLAS_PEDRIN_14.zip
+  wget https://download853.mediafire.com/o9e484xbrzag/akvnk26t00q9mhs/8.DOUGLAS_PEDRIN_14.zip
   cp -r 8.DOUGLAS_PEDRIN_14.zip /sdcard
   mv 8.DOUGLAS_PEDRIN_14.zip Downloads
   exit
 elif [ $ch -eq 9 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/5hsa7s4f7q6ghojnkf54c25rvahnbrani342mz3zmbnkfl3daxjtvcq/9.TODDYZIN_MAKER.zip
+  wget https://download1580.mediafire.com/hg88sez6c6ig/ozh6xmtutdxw1oo/9.TODDYZIN_MAKER.zip
   cp -r 9.TODDYZIN_MAKER.zip /sdcard
   mv 9.TODDYZIN_MAKER.zip Downloads
   exit
 elif [ $ch -eq 10 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/5dsa7s455yshx3bciavjempov4iywy566ks4h5c2dprce6wro76c77q/10.TITANS_DATABASE.zip
+  wget https://download852.mediafire.com/vquh719c5ewg/cwoq1vjz2en1qn3/10.TITANS_DATABASE.zip
   cp -r 10.TITANS_DATABASE.zip /sdcard
   mv 10.TITANS_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 11 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/63sa7s457zzwdpjcav64a5hjx4gnvm4qckbw4bgh23p2oxgw26iem4q/11.VFO_10K_SPECIAL.zip
+  wget https://download940.mediafire.com/mye1t85haeig/tmpx1zw6e3ok6ro/11.VFO_10K_SPECIAL.zip
   cp -r 11.VFO_10K_SPECIAL.zip /sdcard
   mv 11.VFO_10K_SPECIAL.zip Downloads
   exit
 elif [ $ch -eq 12 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/6psa7s4a4e7x5pdnav6j65hnvdhoftjhz64penps43lkhiz7exhnoty/12.PUB_DATABASE.zip
+  wget https://download938.mediafire.com/3n3dttp9hx0g/1l27tso0cbq0fhs/12.PUB_DATABASE.zip
   cp -r 12.PUB_DATABASE.zip /sdcard
   mv 12.PUB_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 13 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/7hsa7s4yvawclnjylmqig57o6tt2l7ffhvmyx765df4ofvfivoe4rxi/13.SPAM.DATABASE.zip
+  wget https://download849.mediafire.com/nhb3b6ielw1g/ul1eh1x93uvn64j/13.SPAM.DATABASE.zip
   cp -r 13.SPAM.DATABASE.zip /sdcard
   mv 13.SPAM.DATABASE.zip Downloads
   exit
 elif [ $ch -eq 14 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/y7sa7s6i6j7hbndfa52z655gx7npwp7qkt43d5tsnuhqshuzh6ckx6i/14.CANNIBAL_DXMODS.zip
+  wget https://download1511.mediafire.com/2yqj4bpx4mpg/j7ea3b856vdn6in/14.CANNIBAL_DXMODS.zip
   cp -r 14.CANNIBAL_DXMODS.zip /sdcard
   mv 14.CANNIBAL_DXMODS.zip Downloads
   exit
 elif [ $ch -eq 15 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/yxsa7s6e7myhvmjhbvvma3flv3xa44whp3jkwro34lic2dbevbehk6q/15.SRZORO_CRASH_DATABASE.zip
+  wget https://download1490.mediafire.com/zi8355w6mc4g/comxkkasnm0710t/15.SRZORO_CRASH_DATABASE.zip
   cp -r 15.SRZORO_CRASH_DATABASE.zip /sdcard
   mv 15.SRZORO_CRASH_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 16 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/ytsa7s6f4erwhlrxinqjk47nupea23wznizf2iyeehms3tslmu2hndi/16.DOUGLAS_MODDER_16_DATABASE.zip
+  wget https://download1079.mediafire.com/c8is967xvjrg/2t8cckc3pbotd8i/16.DOUGLAS_MODDER_16_DATABASE.zip
   cp -r 16.DOUGLAS_MODDER_16_DATABASE.zip /sdcard
   mv 16.DOUGLAS_MODDER_16_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 17 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/ylsa7s4q7nyx3krcizyjg77j77noglpcyachx4irpiqjawhhbi2ef5q/17.EZRAMODS_CRASH_BUG.zip
+  wget https://download1320.mediafire.com/b7x79o3zecmg/35xdjig3u3f13fi/17.EZRAMODS_CRASH_BUG.zip
   cp -r 17.EZRAMODS_CRASH_BUG.zip /sdcard
   mv 17.EZRAMODS_CRASH_BUG.zip Downloads
   exit
 elif [ $ch -eq 18 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/zhsa7s4y6qyxrizda5uyeyvjvtzabk6eezuvmnioma5hi52wiic4bda/18.HELL.BOY_DATABASES.zip
+  wget https://download947.mediafire.com/56fpuu4iacrg/ociqet80hkwolfl/18.HELL.BOY_DATABASES.zip
   cp -r 18.HELL.BOY_DATABASES.zip /sdcard
   mv 18.HELL.BOY_DATABASES.zip Downloads
   exit
 elif [ $ch -eq 19 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/yhsa7s447yygnl3gamqj6mhiuwscfq3orgr7k7dj2momigtzamzswsy/19.ABRAAM_MODS_DATABASE.zip
+  wget https://download1591.mediafire.com/1nhe2hhd6ckg/vf97v98craaoskl/19.ABRAAM_MODS_DATABASE.zip
   cp -r 19.ABRAAM_MODS_DATABASE.zip /sdcard
   mv 19.ABRAAM_MODS_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 20 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/2tsa7s4s6uzcnl3min6y4pvhuoasbpmdfekkglhchtliokb5fzbrkqq/20.ABRAAM_CYCLONE_DATABASE.zip
+  wget https://download1339.mediafire.com/5lu1cwsbfuag/vwsoam6r1jj2yfw/20.ABRAAM_CYCLONE_DATABASE.zip
   cp -r 20.ABRAAM_CYCLONE_DATABASE.zip /sdcard
   mv 20.ABRAAM_CYCLONE_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 21 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/2hsa7s4ev42xl2lmlf5z4mn3wfw4hhwuuopl7lyf73xnn4jtgiyxtfy/21.BLACK_WOLF_DATABASE.zip
+  wget https://download1521.mediafire.com/od8tcyo93v3g/jdg3618sbr5ryyy/21.BLACK_WOLF_DATABASE.zip
   cp -r 21.BLACK_WOLF_DATABASE.zip /sdcard
   mv 21.BLACK_WOLF_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 22 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/2dsa7s4h7j7wtl3miaxmannsvxkiukx46yuxmsyv25cqrakhb5f2tfi/22.MARCHETTIBOY_ANONYM_DATABASE.zip
+  wget https://download941.mediafire.com/4ubcsrkl80ag/dq943k58fwy05n3/22.MARCHETTIBOY_ANONYM_DATABASE.zip
   cp -r 22.MARCHETTIBOY_ANONYM_DATABASE.zip /sdcard
   mv 22.MARCHETTIBOY_ANONYM_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 23 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/gxsa7s4i6jycbktna5vio4fxvzgf7ciaanl2u46ozjkoxflzawkeoka/23.NINJA_V8_DATABASE.zip
+  wget https://download1653.mediafire.com/jv43vyh7d8ag/relsb3tz5v92bmn/23.NINJA_V8_DATABASE.zip
   cp -r 23.NINJA_V8_DATABASE.zip /sdcard
   mv 23.NINJA_V8_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 24 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/f7sa7s4f5y2cdkbolj6mm4v7vhk4f5lubmbnqg6aao2iwi4usor4yey/24.DRAGON_WAR_DATABASE.zip
+  wget https://download1653.mediafire.com/2bfsjzbp5lig/79v6e1xejouf3a4/24.DRAGON_WAR_DATABASE.zip
   cp -r 24.DRAGON_WAR_DATABASE.zip /sdcard
   mv 24.DRAGON_WAR_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 25 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/glsa7s4b64zcdizbaqujs25nvkzlbrsx65z73aakqs7djulokqpp62y/25.KROKER_DEL_REZI_ZAP_DATABASE.zip
+  wget https://download1592.mediafire.com/2olhytnn2ggg/4anlk58lt7nsmmp/25.KROKER_DEL_REZI_ZAP_DATABASE.zip
   cp -r 25.KROKER_DEL_REZI_ZAP_DATABASE.zip /sdcard
   mv 25.KROKER_DEL_REZI_ZAP_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 26 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/f3sa7s4zvrycryztiz4mgyfqvn624jf7vnjxyo37rzl5astg6zgkoqy/26.SC_DB_V2.zip
+  wget https://download1327.mediafire.com/qe52z5isfcgg/d4az2wf44zkczl1/26.SC_DB_V2.zip
   cp -r 26.SC_DB_V2.zip /sdcard
   mv 26.SC_DB_V2.zip Downloads
   exit
 elif [ $ch -eq 27 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/fxsa7s427n3xz2zhln5zs4ntunmn63la323nejeo6mo3snbmglsjh6i/27.MAX_DB_V1_DATABASE.zip
+  wget https://download1490.mediafire.com/rvr2e19s5peg/7bo8repemcw7mdw/27.MAX_DB_V1_DATABASE.zip
   cp -r 27.MAX_DB_V1_DATABASE.zip /sdcard
   mv 27.MAX_DB_V1_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 28 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/ftsa7s4d4azwf2laivx4om5l6k7q43vrwpar33443dyzvs4ouocvyoa/28.BGN.TEAM.MODDER_DATABASE.zip
+  wget https://download1336.mediafire.com/azdw7rapamyg/2upw2a86socxfq7/28.BGN.TEAM.MODDER_DATABASE.zip
   cp -r 28.BGN.TEAM.MODDER_DATABASE.zip /sdcard
   mv 28.BGN.TEAM.MODDER_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 29 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/fpsa7s6gvq6cdkzbkaqymnfmwo3qu3mdiox2m47yf65hicgz24x4jfq/29.SUPER_CRASH_RZ_MODDER_CATALOG_DATABASE.zip
+  wget https://download1478.mediafire.com/kcqt0s7noxvg/57vtefcl03ny143/29.SUPER_CRASH_RZ_MODDER_CATALOG_DATABASE.zip
   cp -r 29.SUPER_CRASH_RZ_MODDER_CATALOG_DATABASE.zip /sdcard
   mv 29.SUPER_CRASH_RZ_MODDER_CATALOG_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 30 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/gdsa7s476yqwhllgj4xikp7hwnvraee54iz25dtiz724b6e36urwuia/30.DB_FALLEN_V2_DATABASE.zip
+  wget https://download1653.mediafire.com/7hfuayjpy2qg/ydcaytel0u0hj9j/30.DB_FALLEN_V2_DATABASE.zip
   cp -r 30.DB_FALLEN_V2_DATABASE.zip /sdcard
   mv 30.DB_FALLEN_V2_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 31 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/hpsa7s4tvyugtnjniyx4a45rv74grfupvmk6bdhb5zs763gp3u766hi/31.DTABASE.V8.GOLD.WARMOD.zip
+  wget https://download1325.mediafire.com/j284g5t9i6lg/ndu60y6h51m3dst/31.DTABASE.V8.GOLD.WARMOD.zip
   cp -r 31.DTABASE.V8.GOLD.WARMOD.zip /sdcard
   mv 31.DTABASE.V8.GOLD.WARMOD.zip Downloads
   exit
 elif [ $ch -eq 32 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/ktta7s6cveygtidbifz4k55w75uiuyrlciw2z5gq663bezubkz3mhia/32.TRAVA_ATRASO_ZOLDYCK.zip
+  wget https://download948.mediafire.com/mza7wcbtcagg/vmzuede4fdp7dao/32.TRAVA_ATRASO_ZOLDYCK.zip
   cp -r 32.TRAVA_ATRASO_ZOLDYCK.zip /sdcard
   mv 32.TRAVA_ATRASO_ZOLDYCK.zip Downloads
   exit
 elif [ $ch -eq 33 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/hhsa7s4i6vzxlobtabwzkznw6se6qmt26rdy74ecgr3sf7gnu6r3jwy/33_DEVIL_KILI_VFO_MAZOKKU_666_DATABASE.zip
+  wget https://download1348.mediafire.com/cjeenjtqd0eg/6hz1c2xilwl2ngk/33_DEVIL_KILI_VFO_MAZOKKU_666_DATABASE.zip
   cp -r 33_DEVIL_KILI_VFO_MAZOKKU_666_DATABASE.zip /sdcard
   mv 33_DEVIL_KILI_VFO_MAZOKKU_666_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 34 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/hdsa7s426b2gxojbkz7yc2vj6dflpn6lredfpe5mor6bg53fsud32kq/34.TMC_WA_BUG_STATUS_VIDEO_DATABASE.zip
+  wget https://download1500.mediafire.com/pxfhfyzbjlig/kcdneepbayury4o/34.TMC_WA_BUG_STATUS_VIDEO_DATABASE.zip
   cp -r 34.TMC_WA_BUG_STATUS_VIDEO_DATABASE.zip /sdcard
   mv 34.TMC_WA_BUG_STATUS_VIDEO_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 35 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/a7sa7s6g7arctkrcjv2me25quu6iatidlg63qv75eeods7skmkyg6fa/35.TMC_WA_NEW_CONTACT_BUG_DATABASE.zip
+  wget https://download1523.mediafire.com/68t1izu2umdg/vf8lt18dea0a6qu/35.TMC_WA_NEW_CONTACT_BUG_DATABASE.zip
   cp -r 35.TMC_WA_NEW_CONTACT_BUG_DATABASE.zip /sdcard
   mv 35.TMC_WA_NEW_CONTACT_BUG_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 36 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/bxsa7s437axxzirhi524inftvd5uviqtitnpqzrxvv2itmzza3c5m4a/36.NINJA_TODDY_V11_WAR_DATABASE_.zip
+  wget https://download1076.mediafire.com/97utb9dsaewg/ghk0ibxm7ojhrhp/36.NINJA_TODDY_V11_WAR_DATABASE_.zip
   cp -r 36.NINJA_TODDY_V11_WAR_DATABASE_.zip /sdcard
   mv 36.NINJA_TODDY_V11_WAR_DATABASE_.zip Downloads
   exit
 elif [ $ch -eq 37 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/btsa7s4z4n3xzirtiuvzc4vpvqfburfjjpyg6pttmt7hiex3vfe6wly/37_NINJA_v9_MODDER_FIGHTING_DATABASE.zip
+  wget https://download1582.mediafire.com/793rs9jdyseg/u4uobcy2eudiacb/37_NINJA_v9_MODDER_FIGHTING_DATABASE.zip
   cp -r 37_NINJA_v9_MODDER_FIGHTING_DATABASE.zip /sdcard
   mv 37_NINJA_v9_MODDER_FIGHTING_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 38 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/bpsa7s4d4m3hzmz4bqwzgyvq6v6afpjgj6r3gkbqspkt7hxfg4fi7na/38.NINJA.V18.DTABASE.zip
+  wget https://download1078.mediafire.com/5vrpqtqteuxg/c85ylkrlkb4jiny/38.NINJA.V18.DTABASE.zip
   cp -r 38.NINJA.V18.DTABASE.zip /sdcard
   mv 38.NINJA.V18.DTABASE.zip Downloads
   exit
 elif [ $ch -eq 39 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/blsa7s6f5z2cllzxiz3zemnk6zsansyajjtpebap3glrqol6ullz2di/39.DELAY_LAG_VIRUS_DATABASE_RARE.zip
+  wget https://download936.mediafire.com/ah5ri12g9pgg/g2eozcsahf7ur6u/39.DELAY_LAG_VIRUS_DATABASE_RARE.zip
   cp -r 39.DELAY_LAG_VIRUS_DATABASE_RARE.zip /sdcard
   mv 39.DELAY_LAG_VIRUS_DATABASE_RARE.zip Downloads
   exit
 elif [ $ch -eq 40 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/bhsa7s4x6nygbmzviyqi2y5s6sc6zijchuwnxnhkcrh27loagdgwe6y/40.GRINGO_WAR_MOD_DATABASE_SPECIAL.zip
+  wget https://download1587.mediafire.com/wg7r8o7q16ng/i2yzig9ci702aec/40.GRINGO_WAR_MOD_DATABASE_SPECIAL.zip
   cp -r 40.GRINGO_WAR_MOD_DATABASE_SPECIAL.zip /sdcard
   mv 40.GRINGO_WAR_MOD_DATABASE_SPECIAL.zip Downloads
   exit
 elif [ $ch -eq 41 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/chsa7s454m2x32zsk5zj257p6evip4mig66yq3mnfllzjdie65vjrhy/41_DB_MICKEY_V7.zip
+  wget https://download946.mediafire.com/a2vi31azlkdg/mnidhf8ornaeysu/41_DB_MICKEY_V7.zip
   cp -r 41_DB_MICKEY_V7.zip /sdcard
   mv 41_DB_MICKEY_V7.zip Downloads
   exit
 elif [ $ch -eq 42 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/cdsa7s6bvmvxnojqk57ya4n3vcmdqeaaou5wujosbbhkhi7augxaj3q/42.DATABASE%20NINJA%20V5.zip
-  cp -r 42.DATABASE\ NINJA\ V5.zip /sdcard
-  mv 42.DATABASE\ NINJA\ V5.zip Downloads
+  wget https://download2224.mediafire.com/ukjwoyjh2wog/jt4q2l3kwj2d4ku/42.DATABASE+NINJA+V5.zip
+  cp -r 42.DATABASE+NINJA+V5.zip /sdcard
+  mv 42.DATABASE+NINJA+V5.zip Downloads
   exit
 elif [ $ch -eq 43 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/d7sa7s4e5jzxvy3mkbvzkn7kunu62gofypxhppd3mgz4zvouhid5d4a/43.NINJA_MODDER_V6_ATTACKING_DATABASE.zip
+  wget https://download1492.mediafire.com/z1zhpdnz9bcg/149qw7yrxu0i56k/43.NINJA_MODDER_V6_ATTACKING_DATABASE.zip
   cp -r 43.NINJA_MODDER_V6_ATTACKING_DATABASE.zip /sdcard
   mv 43.NINJA_MODDER_V6_ATTACKING_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 44 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/d3sa7s4auavhjk3glqqyizpjvti4i3bse23covduqeegz5xgb37y5cq/44.DOUGLAS_V13_WAR_MOD_DATABASE.zip
+  wget https://download1594.mediafire.com/2729fmx6uprg/232js7163qu3wo0/44.DOUGLAS_V13_WAR_MOD_DATABASE.zip
   cp -r 44.DOUGLAS_V13_WAR_MOD_DATABASE.zip /sdcard
   mv 44.DOUGLAS_V13_WAR_MOD_DATABASE.zip Downloads
   exit
 elif [ $ch -eq 45 ];then
-  wget https://ba3.apkadmin.com/cgi-bin/dl.cgi/6xsa7s426m2slkj5kexia7vtvvirlr7i5csjb3ae4rsnf6kfyrfl23y/45.MELHOR_DB_TRAVA_WAR_MOD.zip
+  wget https://download1500.mediafire.com/pijva6zgo1fg/e72c1qg9qztsmcy/45.MELHOR_DB_TRAVA_WAR_MOD.zip
   cp -r 45.MELHOR_DB_TRAVA_WAR_MOD.zip /sdcard
   mv 45.MELHOR_DB_TRAVA_WAR_MOD.zip Downloads
   exit
--- install.sh
+++ install.sh
@@ -1,7 +1,21 @@
 #!/usr/bin
 
+echo -e "\e[1;32m please wait"
 apt --fix-broken install
-apt-get install toilet figlet wget
-rm install.sh
+echo
+echo -e "\e[1:32m INSTALLING TOILET"
+echo
+apt-get install toilet
+echo
+echo -e "\e[1;32m INSTALLING FIGLET"
+echo
+apt-get install figlet 
+echo
+echo -e "\e[1;32m INSTALLING WGET"
+apt-get install wget
+echo
+echo "/e[1;34m PLEASE WAIT"
 mkdir Downloads
+sleep 2
+rm install.sh
 exit
